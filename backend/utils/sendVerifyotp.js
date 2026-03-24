b9b906e54b9559c60bd72d5c833dab763fd3946d
Keshav Ghai <keshavghai89@gmail.com>
2026-03-25 01:09:56 +0530

    add comme

--- backend/utils/sendVerifyotp.js
+++ backend/utils/sendVerifyotp.js
@@ -29,7 +29,7 @@
 //     }
 // };
 // module.exports = {sendEmailotp};
-import { Resend } from 'resend';
+const { Resend } = require('resend');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
