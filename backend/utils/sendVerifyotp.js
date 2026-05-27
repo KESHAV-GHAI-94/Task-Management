f378de06f95e3a619cfc7e794e031d73eda4421e
Keshav Ghai <keshavghai89@gmail.com>
2026-05-27 20:48:31 +0530

    feat: implement database connection with Sequelize and add Resend OTP email utility services

--- backend/utils/sendVerifyotp.js
+++ backend/utils/sendVerifyotp.js
@@ -1,27 +1,21 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 const sendEmailotp = async(email,otp)=>{
     try{
-        const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-    });
-    const mailOptions = {
-    from: `"QUICK TASK" <${process.env.EMAIL_USER}>`,
-    to: email,
-    subject: "Registration OTP - QUICK TASK",
-    html: `
-        <h2>Registration  Request</h2>
-        <p>Your One-Time Password (OTP) is:</p>
-        <h1 style="color: blue;">${otp}</h1>
-        <p>This OTP is valid for <b>10 minutes</b>.</p>
-        <p>If you didn’t request this, please ignore this email.</p>
-        `,
-    };
-    await transporter.sendMail(mailOptions);
-    console.log("OTP email sent to :",email);
+        await resend.emails.send({
+            from: "QUICK TASK <onboarding@resend.dev>",
+            to: email,
+            subject: "Registration OTP - QUICK TASK",
+            html: `
+                <h2>Registration Request</h2>
+                <p>Your One-Time Password (OTP) is:</p>
+                <h1 style="color: blue;">${otp}</h1>
+                <p>This OTP is valid for <b>10 minutes</b>.</p>
+                <p>If you didn’t request this, please ignore this email.</p>
+            `,
+        });
+        console.log("OTP email sent to :",email);
     }
     catch(err){
         console.error("email err : ",err);
